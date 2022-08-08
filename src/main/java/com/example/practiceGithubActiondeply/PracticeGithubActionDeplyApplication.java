f5693587d2f0c1738432d3cac1df7b7f7a8e517a
Akkuchan <102145207+Akkuchan@users.noreply.github.com>
2022-08-08 14:41:49 +0900

    Add files via upload

---
+++ src/main/java/com/example/practiceGithubActiondeply/PracticeGithubActionDeplyApplication.java
@@ -0,0 +1,13 @@
+package com.example.practiceGithubActiondeply;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeGithubActionDeplyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeGithubActionDeplyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/practiceGithubActiondeply/PracticeGithubActionDeplyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.practiceGithubActiondeply;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeGithubActionDeplyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
